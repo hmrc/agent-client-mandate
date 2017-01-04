250d3de31fe8a8ed06d74d58da835f8841ceddea
danPudwellOpencast <dan.pudwell@opencastsoftware.com>
2017-01-04 11:10:42 +0000

    mostly copyright updates, fixed mandate status not updating

--- app/uk/gov/hmrc/agentclientmandate/connectors/EmailConnector.scala
+++ app/uk/gov/hmrc/agentclientmandate/connectors/EmailConnector.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -65,7 +65,7 @@ trait EmailConnector extends ServicesConfig with RawResponseReads {
 
 object EmailConnector extends EmailConnector {
   // $COVERAGE-OFF$
-  val sendEmailUri: String = "send-templated-email"
+  val sendEmailUri: String = "hmrc/email"
   val serviceUrl = baseUrl("email")
   val http: HttpGet with HttpPost with HttpPut = WSHttp
   // $COVERAGE-OFF$
