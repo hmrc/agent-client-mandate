d1d60645f711d573b6f03901f5b8a36414760914
danPudwellOpencast <dan.pudwell@opencastsoftware.com>
2017-03-30 15:32:24 +0100

    hmrc test library update

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -19,7 +19,7 @@ private object AppDependencies {
   private val playUrlBindersVersion = "2.1.0"
   private val playConfigVersion = "4.3.0"
   private val domainVersion = "4.1.0"
-  private val hmrcTestVersion = "2.1.0"
+  private val hmrcTestVersion = "2.3.0"
   private val scalaTestVersion = "2.2.6"
   private val pegdownVersion = "1.6.0"
   private val akkaContribVersion = "2.3.4"
