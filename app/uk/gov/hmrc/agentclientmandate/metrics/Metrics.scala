3cd50767db36d7caae192e778f9c261b31a3ffab
Mark Reynolds <mark.reynolds@digital.hmrc.gov.uk>
2018-06-12 11:50:42 +0100

    DDCNLS-2524: Update enum typo uses

--- app/uk/gov/hmrc/agentclientmandate/metrics/Metrics.scala
+++ app/uk/gov/hmrc/agentclientmandate/metrics/Metrics.scala
@@ -38,7 +38,7 @@ trait Metrics {
 object Metrics extends Metrics with MicroserviceMetrics{
   val registry = metrics.defaultRegistry
   val timers = Map(
-    MetricsEnum.GGAAdminAddKnownFacts -> registry.timer("gga-add-known-facts-agent-response-timer"),
+    MetricsEnum.GGAdminAddKnownFacts -> registry.timer("gga-add-known-facts-agent-response-timer"),
     MetricsEnum.EtmpGetDetails -> registry.timer("etmp-get-details-response-timer"),
     MetricsEnum.MaintainAtedRelationship -> registry.timer("etmp-maintain-ated-relationship-response-timer"),
     MetricsEnum.AtedSubscriptionDetails -> registry.timer("etmp-ated-subscription-details-response-timer"),
@@ -60,7 +60,7 @@ object Metrics extends Metrics with MicroserviceMetrics{
   )
 
   val successCounters = Map(
-    MetricsEnum.GGAAdminAddKnownFacts -> registry.counter("gga-add-known-facts-agent-success-counter"),
+    MetricsEnum.GGAdminAddKnownFacts -> registry.counter("gga-add-known-facts-agent-success-counter"),
     MetricsEnum.EtmpGetDetails -> registry.counter("etmp-get-details-success-counter"),
     MetricsEnum.MaintainAtedRelationship -> registry.counter("etmp-maintain-ated-relationship-success-counter"),
     MetricsEnum.AtedSubscriptionDetails -> registry.counter("etmp-ated-subscription-details-success-counter"),
@@ -71,7 +71,7 @@ object Metrics extends Metrics with MicroserviceMetrics{
   )
 
   val failedCounters = Map(
-    MetricsEnum.GGAAdminAddKnownFacts -> registry.counter("gga-add-known-facts-agent-failed-counter"),
+    MetricsEnum.GGAdminAddKnownFacts -> registry.counter("gga-add-known-facts-agent-failed-counter"),
     MetricsEnum.EtmpGetDetails -> registry.counter("etmp-get-details-failed-counter"),
     MetricsEnum.MaintainAtedRelationship -> registry.counter("etmp-maintain-ated-relationship-failed-counter"),
     MetricsEnum.AtedSubscriptionDetails -> registry.counter("etmp-ated-subscription-details-failed-counter"),
