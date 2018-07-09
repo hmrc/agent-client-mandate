159be93ca948ca13045f28205d4aec991cf251f0
Shade Egun <folashade.s.egun@accenture.com>
2018-07-09 16:33:19 +0100

    DDCNLS-4064: Upgraded microservice-bootstrap from 6.9.0 to 6.18.0

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -12,7 +12,7 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val microserviceBootstrapVersion = "6.9.0"
+  private val microserviceBootstrapVersion = "6.18.0"
   private val domainVersion = "4.1.0"
   private val hmrcTestVersion = "2.3.0"
   private val scalaTestVersion = "3.0.5"
