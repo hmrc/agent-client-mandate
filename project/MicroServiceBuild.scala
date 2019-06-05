1e84fe77a2ab2913f5eb0a4320b4eb9f06a95b7f
James Mountain <31282623+jmountain-hmrc@users.noreply.github.com>
2019-06-05 14:37:26 +0100

    Uplifted dependencies for agent-client-mandate

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -13,14 +13,14 @@ private object AppDependencies {
   import play.core.PlayVersion
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.14.0-play-25",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.19.0-play-25",
     ws,
-    "uk.gov.hmrc" %% "microservice-bootstrap" % "10.4.0",
-    "uk.gov.hmrc" %% "domain" % "5.3.0",
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "10.6.0",
+    "uk.gov.hmrc" %% "domain" % "5.6.0-play-25",
     "com.typesafe.akka" %% "akka-contrib" % "2.3.4",
-    "uk.gov.hmrc" %% "mongo-lock" % "6.10.0-play-25",
+    "uk.gov.hmrc" %% "mongo-lock" % "6.12.0-play-25",
     "uk.gov.hmrc" %% "play-scheduling" % "6.0.0",
-    "uk.gov.hmrc" %% "auth-client" % "2.16.0-play-25"
+    "uk.gov.hmrc" %% "auth-client" % "2.22.0-play-25"
   )
 
   trait TestDependencies {
@@ -31,12 +31,12 @@ private object AppDependencies {
   object Test {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
-        "uk.gov.hmrc" %% "hmrctest" % "3.5.0-play-25" % scope,
+        "uk.gov.hmrc" %% "hmrctest" % "3.9.0-play-25" % scope,
         "org.scalatest" %% "scalatest" % "3.0.5" % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
         "org.pegdown" % "pegdown" % "1.6.0" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "uk.gov.hmrc" %% "reactivemongo-test" % "4.8.0-play-25" % scope,
+        "uk.gov.hmrc" %% "reactivemongo-test" % "4.14.0-play-25" % scope,
         "com.typesafe.akka" % "akka-testkit_2.11" % "2.5.21" % scope,
         "org.mockito" % "mockito-core" % "2.24.5" % scope,
         "org.scalacheck" %% "scalacheck" % "1.14.0" % scope
