30c6f9def20fd27fc6765b8041b3411fa7e9669a
nadinJ <63252254+nadinJ@users.noreply.github.com>
2020-07-10 15:37:28 +0100

    DL-3764: Simple-reactivemongo 7.30.0 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,12 +6,12 @@ object AppDependencies {
   import play.sbt.PlayImport._
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.26.0-play-26",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.30.0-play-26",
     "com.typesafe.akka" %% "akka-actor" % "2.5.23" force(),
     ws,
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.7.0",
     "uk.gov.hmrc" %% "domain" % "5.8.0-play-26",
-    "uk.gov.hmrc" %% "mongo-lock" % "6.21.0-play-26",
+    "uk.gov.hmrc" %% "mongo-lock" % "6.23.0-play-26",
     "uk.gov.hmrc" %% "play-scheduling" % "7.4.0-play-26",
     "uk.gov.hmrc" %% "auth-client" % "3.0.0-play-26",
     "com.typesafe.play" %% "play-json-joda" % "2.6.10"
@@ -30,7 +30,7 @@ object AppDependencies {
         "org.scalatestplus.play"  %% "scalatestplus-play" % "3.1.2"             % scope,
         "org.pegdown"              % "pegdown"            % "1.6.0"             % scope,
         "com.typesafe.play"       %% "play-test"          % PlayVersion.current % scope,
-        "uk.gov.hmrc"             %% "reactivemongo-test" % "4.19.0-play-26"    % scope,
+        "uk.gov.hmrc"             %% "reactivemongo-test" % "4.21.0-play-26"    % scope,
         "com.typesafe.akka"       %% "akka-testkit"       % "2.5.23"            % scope,
         "org.mockito"              % "mockito-core"       % "2.24.5"            % scope,
         "org.scalacheck"          %% "scalacheck"         % "1.14.0"            % scope,
