e8b320bb898d5fd91a7988d175c85a4f74445aac
Matthew-George-Tyler <181731227+Matthew-George-Tyler@users.noreply.github.com>
2025-08-26 13:44:12 +0100

    DL-17036 Fix vulnerabilities reported by MDTP

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,12 @@ private object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % "2.6.0",
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % "9.13.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % "10.1.0",
     "uk.gov.hmrc"       %% "domain-play-30"            % "11.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % "9.13.0",
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % "10.1.0",
     "org.apache.pekko"  %  "pekko-testkit_2.13"     % "1.0.3",
     "org.scalatestplus" %% "scalacheck-1-17"        % "3.2.18.0"
   ).map(_ % Test)
