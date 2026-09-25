a40d5e20c98dda3b7d275be9e1aea225784a5561
Olafisoye <molafisoye@gmail.com>
2026-09-25 11:40:20 +0100

    DL-18256 update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt.*
 private object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % "2.13.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % "2.14.0",
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % "10.7.0",
     "uk.gov.hmrc"       %% "domain-play-30"            % "11.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % "10.7.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % "2.13.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % "2.14.0",
     "org.apache.pekko"  %% "pekko-testkit"          % "1.0.3",
     "org.scalatestplus" %% "scalacheck-1-17"        % "3.2.18.0"
   ).map(_ % Test)
